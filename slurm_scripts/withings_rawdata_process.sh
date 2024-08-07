b8eacaa70f98757c466d4231d0f3e3cf96255163
Neelay <nshah@simonsfoundation.org>
2024-08-07 10:15:48 -0400

    python-3.11 venv

--- slurm_scripts/withings_rawdata_process.sh
+++ slurm_scripts/withings_rawdata_process.sh
@@ -7,4 +7,4 @@
 #SBATCH --cpus-per-task=28  # Adjust if each task requires multiple CPUs
 
 
-srun /mnt/home/nshah/venv-test/bin/python /mnt/home/nshah/Simons-sleep-DM/python_scripts/withing_rawdata_process.py -d /mnt/home/nshah/ceph/Sleep_study-neelay/SubjectsData/data_share_test -o /mnt/ceph/users/info/datasets/sleep_data/aug_data_release_30_families --workers 26
+srun /mnt/home/nshah/sleep-venv/bin/python /mnt/home/nshah/Simons-sleep-DM/python_scripts/withing_rawdata_process.py -d /mnt/home/nshah/ceph/Sleep_study-neelay/SubjectsData/data_share_test -o /mnt/ceph/users/info/datasets/sleep_data/aug_data_release_30_families --workers 26
